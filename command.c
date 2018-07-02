bb535018d7d3731cc9d6623904c75ab319968ee7
Mingqi <han405@purdue.edu>
2018-07-01 23:04:28 -0400

    Commit lab 3

--- command.c
+++ command.c
@@ -206,7 +206,7 @@ void command_execute(command *command) {
   int fdout;
   int ret;
   int i;
-  int tmperr = dup(0);
+  int tmperr = dup(2);
   int fderr;
   if (command->num_simple_commands == 0) {
     prompt();
@@ -220,7 +220,7 @@ void command_execute(command *command) {
     fdin = dup(tmpin);
   }
 
-  /* if (command->err_file) {
+   if (command->err_file) {
     if (command->is_append == 1) {
       fderr = open(command->err_file, O_CREAT | O_RDWR | O_APPEND, 0666);
       if (fderr < 0) {
@@ -236,7 +236,7 @@ void command_execute(command *command) {
   }
   else {
     fderr = dup(tmperr);
-    }*/
+  }
   
   
   for (i = 0; i < command->num_simple_commands; i++) {
