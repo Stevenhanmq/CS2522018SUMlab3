8d9a231f51dfac91f753c7cbd98831690fd3d171
Mingqi <han405@purdue.edu>
2018-07-01 21:22:16 -0400

    Commit lab 3

--- command.c
+++ command.c
@@ -221,7 +221,7 @@ void command_execute(command *command) {
   }
 
   for (i = 0; i < command->num_simple_commands; i++) {
-    dup2(fdin, 0);
+    fdin = dup(0);
     close(fdin);
     if (i == command->num_simple_commands -1) {
       if (command->out_file) {
