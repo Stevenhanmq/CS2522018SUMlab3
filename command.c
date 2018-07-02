a8a47873f83726c77f16c53405913dfd1bf573a8
Mingqi <han405@purdue.edu>
2018-07-01 20:40:46 -0400

    Commit lab 3

--- command.c
+++ command.c
@@ -245,7 +245,8 @@ void command_execute(command *command) {
     close(fdout);
     ret = fork();
     if (ret == 0) {
-      execvp(simple_commands[i].args[0], simple[i].args);
+      execvp(command->simple_commands[i].args[0],
+	     command->simple_command[i].args);
       perror("execvp");
       exit(1);
     }
