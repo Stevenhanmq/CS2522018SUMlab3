ba18ea7d0d71db9cea9dc897ffb29e8bb3ad5a4e
Mingqi <han405@purdue.edu>
2018-07-06 21:31:45 -0400

    Commit lab 3

--- command.c
+++ command.c
@@ -264,7 +264,7 @@ void command_execute(command *command) {
       fdin = fdpipe[0];
     }
     if (strcmp(command->simple_commands[i]->arguments[0],"setenv") == 0) {
-      pid = setenv(command->simple_commands[i]->arguments[1],
+      int pid = setenv(command->simple_commands[i]->arguments[1],
 		   command->simple_commands[i]->arguments[2],1);
       prompt();
       clear();
